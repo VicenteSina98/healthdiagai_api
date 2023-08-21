2c772b8d8d21468350df17fd161fdf941664f3b0
VicenteSina98 <133327651+VicenteSina98@users.noreply.github.com>
2023-08-21 04:05:50 -0400

    💬 prompt actualizado

--- rest_api/functions.py
+++ rest_api/functions.py
@@ -2,36 +2,71 @@ from rest_framework import status
 from .secret_key import API_KEY
 import openai
 import pprint
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 openai.api_key = API_KEY
 
 
-def generar_informacion_2D(data: dict, ficha_medica: str, mensaje: str) -> str:
-    """agrega la informacion almacenada en data a la ficha medica
+def generar_parte_inicial() -> str:
+    """Le da un rol, la presentacion de la herramienta (ficha medica) y que hacer
 
     Args:
-        data (dict): informacion a agregar a la ficha medica
         ficha_medica (str): ficha medica
-        mensaje (str): frase para presentar la informacion
 
     Returns:
-        str: ficha medica con la informacion actualizada
+        str: ficha medica con las instrucciones agregadas
     """
-    flag_iteracion = True
-    for key, value in data.items():
-        if value != None:
-            if flag_iteracion:
-                # primera iteracion
-                ficha_medica += mensaje + key + ': ' + str(value)
-                flag_iteracion = False
-            else:
-                ficha_medica += '; ' + key + ': ' + str(value)
-    if not flag_iteracion:
-        ficha_medica += '. '
+    ficha_medica = 'Eres un médico general profesional.'
+    ficha_medica += ' Te daré los antecedentes médicos de uno de tus pacientes que acudió a una cita médica.'
+    ficha_medica += ' Tu tarea es, dados los antecedentes de tu paciente, dar el diagnostico de las 5 patologías con mayor probabilidad de que el paciente las pueda padecer, ordenadas por probabilidad, junto con a qué profesionales recurrir y qué medicamentos utilizar según cada patología.'
+    ficha_medica += ' Los antecedentes médicos de tu paciente son los siguientes: '
     return ficha_medica
 
 
-def generar_antecedentes_medicos(data: dict) -> str:
+def calcular_edad(fecha_nacimiento: str) -> str:
+    """Calcula la edad de una persona segun fecha de nacimiento
+
+    Args:
+        fecha_nacimiento (str): fecha de nacimiento de una persona
+
+    Returns:
+        str: edad de la persona
+    """
+    fecha_nacimiento_split = fecha_nacimiento.split('-')
+    year = int(fecha_nacimiento_split[0])
+    mes = int(fecha_nacimiento_split[2])
+    dia = int(fecha_nacimiento_split[1])
+    edad = relativedelta(datetime.now(), datetime(year, dia, mes))
+    return str(edad.years)
+
+
+def concatenar_antecedentes_medicos(ficha_medica: str, antecedentes_medicos: dict) -> str:
+    if len(antecedentes_medicos['enfermedades_cronicas']) > 0:
+        ficha_medica += 'Padece de las siguientes enfermedades crónicas: '
+        ficha_medica += antecedentes_medicos['enfermedades_cronicas'] + '. '
+    if len(antecedentes_medicos['historial_alergias']) > 0:
+        ficha_medica += 'Es alérgico a: '
+        ficha_medica += antecedentes_medicos['historial_alergias'] + '. '
+    if len(antecedentes_medicos['historial_cirugias']) > 0:
+        ficha_medica += 'Ha sido sometido a las siguientes cirugías: '
+        ficha_medica += antecedentes_medicos['historial_cirugias'] + '. '
+    if len(antecedentes_medicos['historial_enfermedades_familia']) > 0:
+        ficha_medica += 'En su familia es recurrente padecer las siguientes enfermedades: '
+        ficha_medica += antecedentes_medicos['historial_enfermedades_familiar'] + '. '
+    if len(antecedentes_medicos['historial_enfermedades_infecciosas']) > 0:
+        ficha_medica += 'Padece de las siguientes enfermedades infecciosas: '
+        ficha_medica += antecedentes_medicos['historial_enfermedades_infecciosas'] + '. '
+    if len(antecedentes_medicos['historial_habitos_salud']) > 0:
+        ficha_medica += 'Mantiene los siguientes hábitos de salud: '
+        ficha_medica += antecedentes_medicos['historial_habitos_salud'] + '. '
+    if len(antecedentes_medicos['historial_medicamentos']) > 0:
+        ficha_medica += 'Además, ha consumido a lo largo de su vida los siguientes medicamentos: '
+        ficha_medica += antecedentes_medicos['historial_medicamentos'] + '. '
+    return ficha_medica
+
+
+def generar_antecedentes_medicos(data: dict, ficha_medica: str) -> str:
     """agrega la informacion personal a la ficha medica
 
     Args:
@@ -40,54 +75,26 @@ def generar_antecedentes_medicos(data: dict) -> str:
     Returns:
         str: ficha medica con la informacion personal agregada
     """
-    personalInfo = data['informacion_personal']
-    ficha_medica = 'A continuación se presenta la ficha médica de un paciente. Su nombre es ' + \
-        str(personalInfo['nombres']) + ' ' + str(personalInfo['primer_apellido']) + ' ' + \
-        str(personalInfo['segundo_apellido'])
+    info_personal = data['informacion_personal']['informacion_personal']
+    ant_medicos = data['informacion_personal']['antecedentes_medicos']
+    ficha_medica += str(info_personal['nombres']) + ' '
+    ficha_medica += str(info_personal['primer_apellido']) + ' '
+    ficha_medica += str(info_personal['segundo_apellido']) + ', '
     sexo = ''
-    if personalInfo['sexo'] == 'M':
-        sexo = 'Masculino'
+    if info_personal['sexo'] == 'M':
+        sexo = 'hombre'
     else:
-        sexo = 'Femenino'
-    ficha_medica += ', de sexo ' + sexo
-    ficha_medica += ', nacido el ' + str(personalInfo['fecha_nacimiento'])
-    ficha_medica += ', ' + str(personalInfo['altura']) + ' metros de altura y '
-    ficha_medica += str(personalInfo['peso']) + ' kilogramos de peso.'
-    ficha_medica = generar_informacion_2D(
-        personalInfo['antecedentes_medicos'], ficha_medica, ' Sus antecedentes médicos son los siguientes. ')
+        sexo = 'mujer'
+    
+    ficha_medica += sexo + ' de ' + calcular_edad(str(info_personal['fecha_nacimiento'])) + ', '
+    ficha_medica += ', ' + str(info_personal['altura']) + ' metros de altura y '
+    ficha_medica += str(info_personal['peso']) + ' kilogramos de peso. '
+    ficha_medica = concatenar_antecedentes_medicos(ficha_medica, ant_medicos)
     return ficha_medica
 
 
-def generar_informacion_2D_booleana(data: dict, ficha_medica: str, mensaje: str) -> str:
-    """agrega la informacion almacenada en data a la ficha medica
-
-    Args:
-        data (dict): informacion a agregar a la ficha medica
-        ficha_medica (str): ficha medica
-        mensaje (str): frase para presentar la informacion
-
-    Returns:
-        str: ficha medica con la informacion actualizada
-    """
-    flag_iteracion = True
-    for key, value in data.items():
-        if value != None and value:
-            if key == 'otros':
-                if flag_iteracion:
-                    # primera iteracion
-                    ficha_medica += mensaje + key + ': ' + value
-                    flag_iteracion = False
-                else:
-                    ficha_medica += '; ' + key + ': ' + value
-            else:
-                if flag_iteracion:
-                    # primera iteracion
-                    ficha_medica += mensaje + key
-                    flag_iteracion = False
-                else:
-                    ficha_medica += ',  ' + key
-    if not flag_iteracion:
-        ficha_medica += '. '
+def generar_parte_intermedia(ficha_medica: str) -> str:
+    ficha_medica += ' La información recopilada en tu cita con el paciente fue la siguiente. '
     return ficha_medica
 
 
@@ -124,7 +131,7 @@ def generar_contacto_enfermo(data: dict, ficha_medica: str) -> str:
         ficha_medica += 'El diagnostico del enfermo es el siguiente: ' + \
             data['diagnostico'] + '. '
     if data['sintomas_relacionados'] != None:
-        ficha_medica += 'Los sintomas relacionados son: ' + \
+        ficha_medica += 'Los sintomas relacionados con los sintomas del paciente son: ' + \
             data['sintomas_relacionados'] + '. '
     return ficha_medica
 
@@ -166,20 +173,6 @@ def generar_viaje_extranjero(data: dict, ficha_medica: str) -> str:
     return ficha_medica
 
 
-def generar_parte_final(ficha_medica: str) -> str:
-    """agrega las instrucciones para que ChatGPT genere la prediccion
-
-    Args:
-        ficha_medica (str): ficha medica
-
-    Returns:
-        str: ficha medica con las instrucciones agregadas
-    """
-    ficha_medica += 'Necesito que, según la ficha médica presentada, y me des un listado de las 5 enfermedades con mas probabilidad de que el paciente esté enfermo, junto a una breve explicación del porqué es posible que tenga dicha enfermedad. '
-    ficha_medica += 'Además, necesito que me des un listado de los profesionales del área de la salud a los cuales el paciente puede recurrir para atenderse según su ficha médica y las 5 posibles enfermedades pedidas anteriormente.'
-    return ficha_medica
-
-
 def generar_ficha_medica(data: dict) -> str:
     """genera la ficha medica del paciente
 
@@ -190,22 +183,19 @@ def generar_ficha_medica(data: dict) -> str:
         str: ficha medica generada
     """
     pprint.pprint(data)
-    ficha_medica = generar_antecedentes_medicos(data)
-    ficha_medica = generar_informacion_simple(data, 'alergias_medicamentos',
-                                              ficha_medica, 'El paciente es alergico a los siguientes medicamentos: ')
-    ficha_medica = generar_informacion_simple(data, 'alergias_alimentos',
-                                              ficha_medica, 'El paciente es alergico a los siguientes alimentos: ')
+    ficha_medica = generar_parte_inicial()
+    ficha_medica = generar_antecedentes_medicos(data, ficha_medica)
+    ficha_medica = generar_parte_intermedia(ficha_medica)
     ficha_medica = generar_informacion_simple(data, 'sintomas',
                                               ficha_medica, 'El paciente presenta los siguientes sintomas: ')
     ficha_medica = generar_informacion_simple(data, 'consumo_medicamentos',
-                                              ficha_medica, 'El paciente consume los siguientes medicamentos: ')
+                                              ficha_medica, 'El paciente consume, de manera regular, los siguientes medicamentos: ')
     ficha_medica = generar_informacion_simple(data, 'estado_animo',
                                               ficha_medica, 'El paciente presenta los siguiente estados de animo: ')
     ficha_medica = generar_contacto_enfermo(
         data['contacto_enfermo'], ficha_medica)
     ficha_medica = generar_viaje_extranjero(
         data['viaje_extranjero'], ficha_medica)
-    ficha_medica = generar_parte_final(ficha_medica)
     return ficha_medica
 
 
