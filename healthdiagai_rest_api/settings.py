88cc4ad1f6a99100223930eaac04fbe4634e7ef5
VicenteSina98 <133327651+VicenteSina98@users.noreply.github.com>
2023-10-23 16:54:38 -0300

    otro intento de solucionar cors

--- healthdiagai_rest_api/settings.py
+++ healthdiagai_rest_api/settings.py
@@ -38,10 +38,10 @@ PORT_DB = os.getenv("PORT_DB")
 HOST_API = os.getenv('HOST_API')
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # CSRF_TRUSTED_ORIGINS = [HOST_API]
 # CORS_ORIGIN_WHITELIST = [HOST_API]
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
